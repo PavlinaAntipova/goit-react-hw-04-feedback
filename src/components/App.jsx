976850cdca3d610bce76627891d82492823cca48
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-03-26 16:29:41 +0200

    initial setting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,6 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
+    <div>
       React homework template
     </div>
   );
